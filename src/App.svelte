59728a372ba0ec25c1bdba1138013b7fcbf6ab3b
Simon Weller <s.meierbeck@gmail.com>
2020-06-24 23:46:07 +0200

    set up basic board

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Board from "./Board.svelte";
+  export let name;
+</script>
+
+<main>
+  <h1>Minesweeper</h1>
+  <p>Sweep those mines</p>
+  <Board />
+</main>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
---
+++ src/Board.svelte
@@ -0,0 +1,28 @@
+<script>
+  const createRow = () => Array.from({ length: 9 }, () => 0);
+  let rows = Array.from({ length: 9 }, createRow);
+  console.log(rows);
+</script>
+
+<div class="board">
+  {#each rows as row}
+    <div class="row">
+      {#each row as cell}
+        <div class="cell" />
+      {/each}
+    </div>
+  {/each}
+</div>
+
+<style>
+  .row {
+    display: flex;
+  }
+
+  .cell {
+    height: 30px;
+    width: 30px;
+    background: antiquewhite;
+    border: 2px solid cadetblue;
+  }
+</style>
