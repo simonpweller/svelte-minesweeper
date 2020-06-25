0e14f96244c0b74ca4c83c3fb91fb1640d3961a6
Simon Weller <s.meierbeck@gmail.com>
2020-06-25 23:49:59 +0200

    randomly place mines

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,22 @@
 <script>
-  import Board from "./Board.svelte";
-  export let name;
+  import generate from "./generate";
+  const rows = generate();
 </script>
 
 <main>
   <h1>Minesweeper</h1>
   <p>Sweep those mines</p>
-  <Board />
+  <div class="board">
+    {#each rows as row}
+      <div class="row">
+        {#each row as cell}
+          <div class="cell" class:bomb={cell.isBomb}>
+            {#if !cell.isBomb && cell.bombCount}{cell.bombCount}{/if}
+          </div>
+        {/each}
+      </div>
+    {/each}
+  </div>
 </main>
 
 <style>
@@ -14,5 +24,26 @@
     display: flex;
     flex-direction: column;
     align-items: center;
+  }
+
+  .row {
+    display: flex;
+  }
+
+  .cell {
+    height: 30px;
+    width: 30px;
+    background: #c7bfc7;
+    border: 3px solid #ffffff;
+    border-right-color: #877f87;
+    border-bottom-color: #877f87;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .bomb {
+    background-image: url("/mine.png");
+    background-size: contain;
   }
 </style>
---
+++ src/generate.js
@@ -0,0 +1,42 @@
+export default function generate() {
+  let rows = Array.from({ length: 9 }, (_, rowIndex) => createRow(rowIndex));
+  let cells = rows.reduce((cells, row) => [...cells, ...row], []);
+  placeBombs(cells, 9);
+
+  cells.forEach((cell) => {
+    return (cell.bombCount = getBombCount(rows, cell.rowIndex, cell.colIndex));
+  });
+
+  return rows;
+}
+
+function createRow(rowIndex) {
+  return Array.from({ length: 9 }, (_, colIndex) => ({
+    rowIndex,
+    colIndex,
+    isBomb: false,
+    bombCount: 0,
+  }));
+}
+
+function placeBombs(cells, bombsToPlace) {
+  if (bombsToPlace === 0) return cells;
+  const randomCellIndex = Math.floor(Math.random() * cells.length);
+  if (cells[randomCellIndex].isBomb) return placeBombs(cells, bombsToPlace);
+  cells[randomCellIndex].isBomb = true;
+  return placeBombs(cells, bombsToPlace - 1);
+}
+
+function getBombCount(rows, rowIndex, colIndex) {
+  let bombCount = 0;
+  for (let rowOffset = -1; rowOffset <= 1; rowOffset++) {
+    if (!rows[rowIndex + rowOffset]) continue;
+    for (let colOffset = -1; colOffset <= 1; colOffset++) {
+      const neighbour = rows[rowIndex + rowOffset][colIndex + colOffset];
+      if (neighbour && neighbour.isBomb) {
+        bombCount++;
+      }
+    }
+  }
+  return bombCount;
+}
