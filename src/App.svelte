451c22736b52be8c5133461d6eaed1d52832632c
Simon Weller <s.meierbeck@gmail.com>
2020-06-27 20:58:22 +0200

    add game state

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import generate from "./generate";
   import Cell from "./Cell.svelte";
   let rows = generate();
+  let gameState = "playing";
 
   function reveal(cell) {
     if (!cell.covered || cell.flagged) return;
@@ -9,6 +10,7 @@
     if (cell.bombCount === 0) {
       getNeighbours(cell).forEach(reveal);
     }
+    updateGameState(cell);
   }
 
   function toggleFlag(cell) {
@@ -16,6 +18,23 @@
     rows[cell.rowIndex][cell.colIndex].flagged = !rows[cell.rowIndex][
       cell.colIndex
     ].flagged;
+  }
+
+  function updateGameState() {
+    gameState = getGameState();
+  }
+
+  function getGameState() {
+    let coveredCells = 0;
+    let bombs = 0;
+    for (const row of rows) {
+      for (const cell of row) {
+        if (cell.bomb && !cell.covered) return "lost";
+        if (cell.covered) coveredCells++;
+        if (cell.bomb) bombs++;
+      }
+    }
+    return coveredCells === bombs ? "won" : "playing";
   }
 
   function getNeighbours(cell) {
@@ -32,15 +51,22 @@
 
 <main>
   <h1>Minesweeper</h1>
-  <p>Sweep those mines</p>
+  {#if gameState === 'playing'}
+    <p>Sweep those mines</p>
+  {:else if gameState === 'won'}
+    <p>You won!</p>
+  {:else if gameState === 'lost'}
+    <p>You lost!</p>
+  {/if}
+
   <div class="board">
     {#each rows as row}
       <div class="row">
         {#each row as cell}
           <Cell
             {cell}
-            on:reveal={() => reveal(cell)}
-            on:toggle-flag={() => toggleFlag(cell)} />
+            on:reveal={() => gameState === 'playing' && reveal(cell)}
+            on:toggle-flag={() => gameState === 'playing' && toggleFlag(cell)} />
         {/each}
       </div>
     {/each}
